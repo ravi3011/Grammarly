c5be5b308996cb8bc3c0feeaab76295de76ebb2b
rk6025 <ravi@achieverspoint.co>
2020-12-05 13:16:52 +0530

    api call for dat get bas=d words

--- app.js
+++ app.js
@@ -5,5 +5,18 @@ function ReadFile(files){
     reader.readAsText(file);	
     reader.onload = function(){	
         filecontent.value = reader.result;	
-    }	
-}	
+    }
+    getCheck(reader.result);	
+}
+
+async function getCheck(data){
+    let url = "https://api.textgears.com/grammar?key=wF4nKAm0SzmgBeEd&text="+data;
+    let response = await fetch(url);
+    if(response.ok){
+        let spell = await response.json();
+        console.log(spell);
+        // for(let item of spell){
+        //     console.log(item);
+        // }
+    }
+}
